2e31eccae2760dcb1365e2bac16458d3648a7072
MrFixThis <mrfixthis55@gmail.com>
2022-08-25 13:12:50 -0500

    config(toggleterm): floating win's border color changed

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,3 +1,4 @@
+local scheme = require("nebulous.functions").get_colors("midnight")
 require("toggleterm").setup({
   -- size can be a number or function which is passed the current terminal
   size = 20,
@@ -15,6 +16,11 @@ require("toggleterm").setup({
   close_on_exit = true, -- close the terminal window when the process exits
   shell = vim.o.shell, -- change the default shell
   auto_scroll = true, -- automatically scroll to the bottom on terminal output
+  highlights = {
+    FloatBorder = {
+      guifg = scheme.DarkOrange
+    },
+  },
   float_opts = {
     border = "curved",
     winblend = 0,
