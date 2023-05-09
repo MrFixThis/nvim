a89eadeab13ac1e5cf959299ac2281a19f0ca95e
MrFixThis <mrfixthis55@gmail.com>
2023-05-09 14:28:52 -0500

    config(chatgpt.nvim): configs updated.

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -85,21 +85,19 @@ return {
       "MunifTanjim/nui.nvim",
     },
     opts = {
-      chat = {
-        keymaps = {
-           close = { "<C-c>" },
-           yank_last = "<C-y>",
-           yank_last_code = "<C-k>",
-           scroll_up = "<C-u>",
-           scroll_down = "<C-d>",
-           toggle_settings = "<C-o>",
-           new_session = "<C-n>",
-           cycle_windows = "<Tab>",
-           -- in the Sessions pane
-           select_session = "s",
-           rename_session = "r",
-           delete_session = "d",
-         },
+      keymaps = {
+         close = { "<C-c>" },
+         yank_last = "<C-y>",
+         yank_last_code = "<C-k>",
+         scroll_up = "<C-u>",
+         scroll_down = "<C-d>",
+         toggle_settings = "<C-o>",
+         new_session = "<C-n>",
+         cycle_windows = "<Tab>",
+         -- in the Sessions pane
+         select_session = "s",
+         rename_session = "r",
+         delete_session = "d",
       },
       popup_input = {
         submit = "<CR>",
