1a8f013e1150c7f66df846524a206e6a7e115353
MrFixThis <mrfixthis55@gmail.com>
2023-09-27 07:39:27 -0500

    chore(tools): Remove useless plugins

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -223,57 +223,6 @@ return {
     end,
   },
 
-  -- Rest-nvim
-  {
-    "NTBBloodbath/rest.nvim",
-    ft = "http",
-    opts = {
-      result_split_horizontal = false,
-      result_split_in_place = "left",
-      skip_ssl_verification = false,
-      highlight = {
-        enabled = true,
-        timeout = 150,
-      },
-      result = {
-        show_url = true,
-        show_http_info = true,
-        show_headers = true,
-        formatters = {
-          json = "jq",
-          html = function(body)
-            return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-          end
-        },
-      },
-      jump_to_request = false,
-      env_file = ".env",
-      custom_dynamic_variables = {},
-      yank_dry_run = true,
-    },
-    config = function(_, opts)
-      local rest_nvim = require("rest-nvim")
-      rest_nvim.setup(opts)
-      set_keymap({
-        {
-          "n", "<leader>rr",
-          rest_nvim.run,
-          { buffer = true, desc = "Rest.nvim run" }
-        },
-        {
-          "n", "<leader>rR",
-          function() rest_nvim.run(true) end,
-          { buffer = true, desc = "Rest.nvim re-run" }
-        },
-        {
-          "n", "<leader>rf",
-          rest_nvim.last,
-          { buffer = true, desc = "Rest.nvim run last" }
-        },
-      })
-   end,
-  },
-
   -- Markdown preview
   {
     "iamcco/markdown-preview.nvim",
