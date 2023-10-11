ab43f74ce93b269ade5dcc2500ff94c76be5e680
MrFixThis <mrfixthis55@gmail.com>
2023-10-10 22:14:06 -0500

    chore(lsp): Update diagnostics' icons

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -195,7 +195,7 @@ return {
       })
 
       -- Diagnostics
-      local signs = { Error = " ", Warn = " ", Info = " ", Hint = " " }
+      local signs = { Error = " ", Warn = " ", Info = " ", Hint = " " }
       for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
