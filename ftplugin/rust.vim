e6645a8551449879102d99047cf238506c86fc7e
MrFixThis <mrfixthis55@gmail.com>
2023-12-07 10:27:40 -0500

    chore: Apply global format

--- ftplugin/rust.vim
+++ ftplugin/rust.vim
@@ -1,5 +1,5 @@
 setlocal shiftwidth=4
 setlocal tabstop=4
 setlocal softtabstop=4
-setlocal colorcolumn=120
-setlocal textwidth=120
+setlocal colorcolumn=100
+setlocal textwidth=100
--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -331,7 +331,7 @@ return {
   {
     'folke/trouble.nvim',
     dependencies = {
-      'nvim-tree/nvim-web-devicons'
+      'nvim-tree/nvim-web-devicons',
     },
     keys = {
       {
